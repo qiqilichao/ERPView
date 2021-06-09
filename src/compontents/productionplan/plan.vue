5aef1065c2534218f05dd7c9b3d8ec84ce0cf3f1
wq0908 <1208761299@qq.com>
2021-06-09 21:04:27 +0800

    tere

--- src/compontents/productionplan/plan.vue
+++ src/compontents/productionplan/plan.vue
@@ -7,8 +7,8 @@
           <el-col :span="9" :offset="15">
           <div>
             <el-button @click="showadd">添加产品</el-button>
-            <el-button>添加删除</el-button>
-            <el-button>提交审核</el-button>
+            <el-button @click="delApply">删除产品</el-button>
+            <el-button @click="addApplyData">提交审核</el-button>
             <el-button>返回</el-button>
           </div>
           </el-col>
@@ -24,46 +24,61 @@
               <el-input placeholder="请输入内容" size="small">
                 <template slot="prepend">供货时间:</template>
               </el-input>
-              <el-input placeholder="请输入内容" size="small">
+              <el-input placeholder="请输入内容" size="small" v-model="register">
                 <template slot="prepend">登记人是:</template>
               </el-input>
-              <el-input placeholder="请输入内容" :value="date" size="small">
+              <el-input placeholder="请输入内容" :value="datez" size="small">
                 <template slot="prepend">登记时间:</template>
               </el-input>
 
             </div>
 
-            <el-table>
+            <el-table :data="applyData">
+              <el-table-column label="点选" width="90">
+                <template slot-scope="scope">
+                  <el-checkbox v-model="scope.row.checked"></el-checkbox>
+                </template>
+              </el-table-column>
               <el-table-column
+                prop="productId"
                 label="产品名称"
                 width="170">
               </el-table-column>
               <el-table-column
+                prop="productName"
                 label="产品编号"
                 width="170">
               </el-table-column>
               <el-table-column
+                prop="productNick"
                 label="描述"
                 width="170">
               </el-table-column>
-              <el-table-column
-                label="数量"
-                width="170">
+
+              <el-table-column label="数量" width="200">
+                <template slot-scope="scope">
+                   <el-input v-model="scope.row.num" placeholder="请输入内容"></el-input>
+                </template>
               </el-table-column>
+
               <el-table-column
+                prop="amountUnit"
                 label="单位"
-                width="170">
+                width="180">
               </el-table-column>
               <el-table-column
+                prop="listPrice"
                 label="单价（元）"
                 width="170">
               </el-table-column>
               <el-table-column
+                prop="listPrice"
                 label="小计（元）"
                 width="170">
               </el-table-column>
 
             </el-table>
+
             <div>
               备注:
               <el-input
@@ -115,48 +130,96 @@
       data() {
         return {
           str: "新发生",
-          date: "",
+          datez: "",
           pagesize:5,
           pageno:1,
           total:0,
           dialogTableVisible: false,
           gridData: [],
+          applyData:[],
+          remark:"",
+          register:"",
+         // checked:false,
+
+
         }
       },
       methods: {
 
         //当前时间获取方法
-        getDate(format) {
-          /**
-           *获取当前时间
-           *format=1精确到天
-           *format=2精确到分
-           */
-          var now = new Date();
-          var year = now.getFullYear(); //得到年份
-          var month = now.getMonth();//得到月份
-          var date = now.getDate();//得到日期
-          var day = now.getDay();//得到周几
-          var hour = now.getHours();//得到小时
-          var minu = now.getMinutes();//得到分钟
-          var sec = now.getSeconds();//得到秒
-          month = month + 1;
-          if (month < 10) month = "0" + month;
-          if (date < 10) date = "0" + date;
-          if (hour < 10) hour = "0" + hour;
-          if (minu < 10) minu = "0" + minu;
-          if (sec < 10) sec = "0" + sec;
-          var time = "";
-          //精确到天
-          if (format == 1) {
-            time = year + "/" + month + "/" + date;
-          }
-          //精确到分
-          else if (format == 2) {
-            time = year + "/" + month + "/" + date + " " + hour + ":" + minu + ":" + sec;
-          }
-          this.date = time;
+        getDate(){
+          let date = new Date();
+          let Y = date.getFullYear();
+          let M = this.getStr(date.getMonth() + 1);
+          let D = this.getStr(date.getDate());
+          let hours = date.getHours();
+          let minutes = this.getStr(date.getMinutes());
+          let seconds = this.getStr(date.getSeconds());
+          var date1 =Y + "/" + M + "/" + D + " " + hours + ":" + minutes + ":" + seconds;
+          this.datez = date1;
         },
+        getStr(point) {
+          return ("00" + point).slice(-2); // 从字符串的倒数第二个字符开始截取，一直截取到最后一个字符；（在这里永远截取该字符串的最后两个字符）
+        },
+
+        //添加Apply数据
+        addApplyData(){
+
+                var newapplydata=this.applyData.filter((tiem)=>{
+                         tiem.registerTime2=new Date();
+                         tiem.register2=this.register;
+                         tiem.remark=this.remark;
+
+                  return tiem.checked==true;
+                });
+
+                if(newapplydata.length==0){
+                  this.$message({
+                    message: '你没有点选产品',
+                    type: 'warning'
+
+                  });
+
+                  return;
+                }
+
+                 this.$confirm('确定提交这个生产计划吗？', '提示', {
+                   confirmButtonText: '确定',
+                   cancelButtonText: '取消',
+                   type: 'warning'
+                 }).then(() => {
+
+                  /* var params=URLSearchParams()
+                       params.append("registerTime",this.date)
+                       params.append("register",this.register)*/
+                   this.$axios.post("apply/addApply",JSON.stringify(newapplydata),
+                     {headers:{"Content-Type":"application/json"}}).then((response)=>{
+
+                         if(response.data==true){
+                           this.delApply()
+                           this.$message({
+                             showClose: true,
+                             message: '提交成功',
+                             type: 'success'
+                           });
+                         }else{
+                           this.$message({
+                             showClose: true,
+                             message: '提交失败',
+                             type: 'error'
+                           });
+                         }
+
+                    }).catch()
+
+                 }).catch(() => {
+                   this.$message({
+                     type: 'info',
+                     message: '已取消提交'
+                   });
+                 });
+        },
+
 
         //获取Dfile表数据
         getDfile(){
@@ -186,7 +249,24 @@
 
         //生产计划添加商品
         addpla(id){
-          alert(id)
+
+             /* var params=new URLSearchParams()
+
+                   params.append("amount",this.amount);
+                   params.append("remark",this.date);
+                   params.append("register",this.register);
+                   params.append("remark",this.remark);*/
+                  var params=new URLSearchParams()
+                   params.append("id",id)
+
+                   this.$axios.post("apply/getByDid",params).then((response)=>{
+                       //显示临时的生产商品数据
+
+                         this.applyData.push(response.data)
+
+
+                    }).catch()
+
         },
 
         handleSizeChange(val) {  //页size变更
@@ -198,9 +278,29 @@
           this.pageno = val;
           this.getDfile();
         },
+
+        //删除产品
+        delApply(){
+
+             var newapplydata=this.applyData.filter((tiem)=>{return tiem.checked==true;});
+
+             if(newapplydata.length==0){
+               this.$message({
+                 message: '请勾选你要删除的产品',
+                 type: 'warning'
+                 });
+               return;
+               }
+             this.applyData.forEach((item,index)=>{
+               if(item.checked==true){
+                 this.applyData.splice(index,newapplydata.length);
+               }
+             })
+        }
+
       },
        created() {
-        this.getDate(2)
+        this.getDate()
        }
 
     }
