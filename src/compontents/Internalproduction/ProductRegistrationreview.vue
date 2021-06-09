0ce3fc96eb9db0997e805ddd3186235b5af4c864
xiaoqiong <3275057945@qq.com>
2021-06-09 14:19:12 +0800

    kk

--- src/compontents/Internalproduction/ProductRegistrationreview.vue
+++ src/compontents/Internalproduction/ProductRegistrationreview.vue
@@ -1,12 +1,56 @@
 <template>
     <div id="ProductRegistrationreview">
-      <h1>生产登记复核</h1>
+      <span>当前等待登记的生产派工单总数:{{total}}</span>
+      <el-table :data="proretable"  border style="width: 100%">
+        <el-table-column
+          prop="id"
+          label="生产派工单编号"
+          width="200">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="产品编号"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="产品名称"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="数量"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="出库单编号集合"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="登记时间"
+          width="175">
+        </el-table-column>
+        <el-table-column label="复核">
+          <template slot-scope="scope">
+            <el-button size="mini"
+                       @click="handleEdit(scope.row.id)">复核</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
 </template>
 
 <script>
     export default {
-        name: "ProductRegistrationreview"
+        name: "ProductRegistrationreview",
+      data(){
+        return {
+          proretable:[],
+          total:0
+        }
+      }
     }
 </script>
 
