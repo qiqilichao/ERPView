65f502e2bdbe90392b3df1808dff5f868cf976a8
xiaoqiong <3275057945@qq.com>
2021-06-10 15:22:57 +0800

    kk

--- src/compontents/Productprocessdesign/Makeprocessdesignsheet.vue
+++ src/compontents/Productprocessdesign/Makeprocessdesignsheet.vue
@@ -40,11 +40,10 @@
       :visible.sync="upddiod"
       width="83%"
       :before-close="handleClose">
-      <div >
+      <div v-show="show1">
         <el-button type="success" @click="upprolist">添加工序</el-button>
         <el-button type="danger" @click="delprolist">删除工序</el-button>
-        <el-button type="primary" @click="">提交</el-button>
-      </div>
+        <el-button type="primary" @click="showUp">预览</el-button>
       <h1>生产工序设计单</h1>
       <el-form :inline="true" :model="updstu" class="demo-form-inline">
         <el-form-item label="产品名称：">
@@ -63,7 +62,7 @@
           label="点选"
           width="70px">
           <template slot-scope="scope">
-            <el-checkbox v-model="scope.row.checked">
+            <el-checkbox  v-model="scope.row.checked">
             </el-checkbox>
           </template>
         </el-table-column>
@@ -86,27 +85,30 @@
           label="工时数"
           width="185">
           <template slot-scope="scope">
-            <el-input v-model="scope.row.labourHourAmount"></el-input>
+            <el-input v-model="scope.row.labourHourAmount" @input="aa(scope.row.id)"></el-input>
           </template>
         </el-table-column>
         <el-table-column
           label="工时单位"
           width="185">
           <template slot-scope="scope">
-            <el-input v-model="scope.row.amountUnit"></el-input>
+            <el-input v-model="scope.row.amountUnit" ></el-input>
           </template>
         </el-table-column>
         <el-table-column
           label="单位工时成本"
           width="190">
           <template slot-scope="scope">
-            <el-input v-model="scope.row.costPrice"></el-input>
+            <el-input v-model="scope.row.costPrice" @input="aa(scope.row.id)"></el-input>
           </template>
         </el-table-column>
         <el-table-column
+          prop="subtotal"
           label="工时成功小计（元）"
           width="190">
-          <span></span>
+<!--          <template slot-scope="scope">-->
+<!--            <span>{{scope.row.subtotal}}</span>-->
+<!--          </template>-->
         </el-table-column>
       </el-table>
       <br/>
@@ -130,6 +132,92 @@
           <el-input   v-model="updstu.sss" style="width: 1000px" type="textarea"></el-input>
         </el-form-item>
       </el-form>
+      </div>
+      <div v-show="show2">
+        <el-button type="primary" @click="">提交</el-button>
+        <el-button type="info" @click="showdwon">返回</el-button>
+        <h1>生产工序设计单</h1>
+        <el-form :inline="true" :model="updstu" class="demo-form-inline">
+          <el-form-item label="产品名称：">
+            <span style="width: 400px">{{updstu.idss}}</span>
+          </el-form-item>
+          <el-form-item style="padding-left: 450px" label="产品编号：">
+            <span>{{updstu.cpid}}</span>
+          </el-form-item><br>
+          <el-form-item label="设计人："  style="width: 386px">
+            <span>{{updstu.qweqwe}}</span>
+          </el-form-item>
+        </el-form>
+        <el-table :data="outapp"  border style="width: 100%">
+          <el-table-column
+            label="点选"
+            width="70px">
+            <template slot-scope="scope">
+              <el-checkbox v-model="scope.row.checked">
+              </el-checkbox>
+            </template>
+          </el-table-column>
+          <el-table-column
+            prop="typeName"
+            label="工序名称"
+            width="185">
+          </el-table-column>
+          <el-table-column
+            prop="typeId"
+            label="工序编号"
+            width="185">
+          </el-table-column>
+          <el-table-column
+            prop="describe1"
+            label="描述"
+            width="185">
+          </el-table-column>
+          <el-table-column
+            prop="labourHourAmount"
+            label="工时数"
+            width="185">
+          </el-table-column>
+          <el-table-column
+            prop="amountUnit"
+            label="工时单位"
+            width="185">
+          </el-table-column>
+          <el-table-column
+            prop="costPrice"
+            label="单位工时成本"
+            width="190">
+          </el-table-column>
+          <el-table-column
+            prop="subtotal"
+            label="工时成功小计（元）"
+            width="190">
+          </el-table-column>
+        </el-table>
+        <br/>
+        <el-form :inline="true" :model="updstu" class="demo-form-inline">
+          <el-form-item label="登记人：">
+            <span>{{updstu.id}}</span>
+          </el-form-item>
+          <el-form-item style="padding-left: 520px" label="登记时间：">
+            <div class="block" >
+              <el-date-picker style="width: 400px"
+                              v-model="updstu.age"
+                              type="datetime"
+                              placeholder="选择日期时间"
+                              default-time="12:00:00"
+                              :picker-options="pickerOptions1"
+                              :disabled="true">
+              </el-date-picker>
+            </div>
+          </el-form-item><br>
+          <el-form-item label="设计要求：">
+<!--            <el-input   v-model="updstu.sss" style="width: 1000px" type="textarea"></el-input>-->
+            <span style="width: 1000px">
+              {{updstu.sss}}
+            </span>
+          </el-form-item>
+        </el-form>
+      </div>
     </el-dialog>
 
     <el-table :data="productlist" border style="width: 100%">
@@ -198,7 +286,9 @@
               }
             },
             sc: false,
-            prolist:[]
+            prolist:[],
+            show1:true,
+            show2:false
           }
       },
       methods:{
@@ -223,23 +313,58 @@
             var a=this.outapp.find(item =>{
               return item.id==ids
             })
-            console.log(a)
             if (a==null){
               this.outapp.push(this.prolist[index]);
             }
           }else {
             this.outapp.push(this.prolist[index]);
-            console.log(this.outapp)
           }
         },
         delprolist(){
-
+          var newoutapp= this.outapp.filter(item => item.checked==true)
+          console.log(newoutapp)
+          if (newoutapp.length!=0){
+          this.outapp.forEach((item,index) => {
+            if(item.checked){
+               this.outapp.splice(index,newoutapp.length)
+            }
+          })
+          }else {
+            this.$message({
+              showClose: true,
+              message: '请选择一条工序删除',
+              type: 'warning'
+            });
+          }
+        },
+        aa(ids){
+          console.log("blur")
+          var row = this.outapp.find((item)=>{return item.id == ids;});
+          if (Number(row.labourHourAmount)!=NaN && Number(row.costPrice) ){
+            row.subtotal = Number(row.labourHourAmount) * Number(row.costPrice);
+          }
+        },
+        showUp(){
+          this.show1=false;
+          this.show2=true;
+          // this.prolist.forEach(item =>{
+          //   item.subtotal = Number(item.labourHourAmount) * Number(item.costPrice);
+          // })
+        },
+        showdwon(){
+          this.show1=true;
+          this.show2=false
         }
       },
       computed:{
-          // gscbxj(){
-          //   // return this.outapp[0].labourHourAmount*this.outapp[0].costPrice
-          // }
+          gscbxj(){
+            console.log(111)
+            var ss= this.outapp.forEach(item =>{
+               return Number(item.labourHourAmount)*Number(item.costPrice)
+              // return item.subtotal
+            })
+            console.log(ss)
+          }
       }
     }
 </script>
