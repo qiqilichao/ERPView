81934860d9e4cd386ff7fdfee407d219ffdd4bf6
xiaoqiong <3275057945@qq.com>
2021-06-15 14:18:08 +0800

    kk

--- src/compontents/Productprocessdesign/Makeprocessdesignsheet.vue
+++ src/compontents/Productprocessdesign/Makeprocessdesignsheet.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="Makeprocessdesignsheet">
-
-    <el-button @click="upddiod=true">sss</el-button>
-
     <el-dialog
       title="添加工序"
       :visible.sync="sc"
@@ -48,13 +45,13 @@
       <el-form :inline="true" :model="updstu" class="demo-form-inline">
         <el-form-item label="产品名称：">
           <!--        <el-input v-model="updstu.id" style="width: 400px"></el-input>-->
-          <span style="width: 400px">{{updstu.idss}}</span>
+          <span style="width: 400px">{{updstu.productName}}</span>
         </el-form-item>
         <el-form-item style="padding-left: 450px" label="产品编号：">
-          <span>{{updstu.cpid}}</span>
+          <span>{{updstu.productId}}</span>
         </el-form-item><br>
         <el-form-item label="设计人：">
-          <el-input v-model="updstu.qweqwe" style="width: 386px" placeholder="请输入设计人"></el-input>
+          <el-input v-model="updstu.designe1" style="width: 386px" placeholder="请输入设计人"></el-input>
         </el-form-item>
       </el-form>
       <el-table :data="outapp"  border style="width: 100%">
@@ -114,22 +111,23 @@
       <br/>
       <el-form :inline="true" :model="updstu" class="demo-form-inline">
         <el-form-item label="登记人：">
-          <el-input v-model="updstu.id" style="width: 400px"></el-input>
+          <el-input v-model="updstu.register1" style="width: 400px"></el-input>
         </el-form-item>
         <el-form-item style="padding-left: 120px" label="登记时间：">
           <div class="block" >
             <!--            <span class="demonstration">设置默认时间</span>-->
             <el-date-picker style="width: 400px"
-                            v-model="updstu.age"
+                            v-model="updstu.registerTime1"
                             type="datetime"
                             placeholder="选择日期时间"
+                            value-format="yyyy-MM-dd HH:mm:ss"
                             default-time="12:00:00"
                             :picker-options="pickerOptions1">
             </el-date-picker>
           </div>
         </el-form-item><br>
         <el-form-item label="设计要求：">
-          <el-input   v-model="updstu.sss" style="width: 1000px" type="textarea"></el-input>
+          <el-input   v-model="updstu.procedureDescribe" style="width: 1000px" type="textarea"></el-input>
         </el-form-item>
       </el-form>
       </div>
@@ -139,13 +137,13 @@
         <h1>生产工序设计单</h1>
         <el-form :inline="true" :model="updstu" class="demo-form-inline">
           <el-form-item label="产品名称：">
-            <span style="width: 400px">{{updstu.idss}}</span>
+            <span style="width: 400px">{{updstu.productName}}</span>
           </el-form-item>
           <el-form-item style="padding-left: 450px" label="产品编号：">
-            <span>{{updstu.cpid}}</span>
+            <span>{{updstu.productId}}</span>
           </el-form-item><br>
           <el-form-item label="设计人："  style="width: 386px">
-            <span>{{updstu.qweqwe}}</span>
+            <span>{{updstu.designer1}}</span>
           </el-form-item>
         </el-form>
         <el-table :data="outapp"  border style="width: 100%">
@@ -196,14 +194,15 @@
         <br/>
         <el-form :inline="true" :model="updstu" class="demo-form-inline">
           <el-form-item label="登记人：">
-            <span>{{updstu.id}}</span>
+            <span>{{updstu.register1}}</span>
           </el-form-item>
           <el-form-item style="padding-left: 520px" label="登记时间：">
             <div class="block" >
               <el-date-picker style="width: 400px"
-                              v-model="updstu.age"
+                              v-model="updstu.registerTime1"
                               type="datetime"
                               placeholder="选择日期时间"
+                              value-format="yyyy-MM-dd HH:mm:ss"
                               default-time="12:00:00"
                               :picker-options="pickerOptions1"
                               :disabled="true">
@@ -213,13 +212,61 @@
           <el-form-item label="设计要求：">
 <!--            <el-input   v-model="updstu.sss" style="width: 1000px" type="textarea"></el-input>-->
             <span style="width: 1000px">
-              {{updstu.sss}}
+              {{updstu.procedureDescribe}}
             </span>
           </el-form-item>
         </el-form>
       </div>
     </el-dialog>
 
+    <el-form :inline="true" :model="Search" class="demo-form-inline">
+      <el-form-item label="一级分类：">
+        <el-select v-model="Search.firstKindId" clearable @change="firest" placeholder="请选择">
+          <el-option
+            v-for="item in firestoption"
+            :key="item.id"
+            :label="item.kindName"
+            :value="item.kindId">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="一级分类：">
+        <el-select v-model="Search.secondKindId" clearable @change="second" placeholder="请选择">
+          <el-option
+            v-for="item in secondoption"
+            :key="item.kindId"
+            :label="item.kindName"
+            :value="item.kindId">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类：">
+        <el-select v-model="Search.thirdKindId" clearable placeholder="请选择">
+          <el-option
+            v-for="item in thirdoption"
+            :key="item.kindId"
+            :label="item.kindName"
+            :value="item.kindId">
+          </el-option>
+        </el-select>
+      </el-form-item><br>
+      <el-form-item>
+        <div class="block">
+          <el-date-picker
+            v-model="datelist"
+            @change="bb"
+            type="daterange"
+            range-separator="至"
+            value-format="yyyy-MM-dd"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期">
+          </el-date-picker>
+        </div>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getdata" type="primary">查询</el-button>
+      </el-form-item>
+    </el-form>
     <el-table :data="productlist" border style="width: 100%">
       <el-table-column
       prop="id"
@@ -227,42 +274,61 @@
       width="230">
       </el-table-column>
       <el-table-column
-        prop="name"
+        prop="productName"
         label="产品名称"
         width="140">
       </el-table-column>
       <el-table-column
-        prop="name"
         label="档次级别"
         width="230">
+        <template slot-scope="scope" v-if="scope.supports">
+            {{scope.row.dfile.productClass | jibie}}
+        </template>
       </el-table-column>
       <el-table-column
-        prop="name"
         label="I级分类"
         width="170">
+        <template slot-scope="scope" v-if="scope.supports">
+          {{scope.row.dfile.firstKindName}}
+        </template>
       </el-table-column>
       <el-table-column
-        prop="name"
         label="II级分类"
         width="160">
+        <template slot-scope="scope" v-if="scope.supports">
+          {{scope.row.dfile.secondKindName}}
+        </template>
       </el-table-column>
       <el-table-column
-        prop="name"
         label="III级分类"
         width="160">
+        <template slot-scope="scope" v-if="scope.supports">
+          {{scope.row.dfile.thirdKindName}}
+        </template>
       </el-table-column>
       <el-table-column
-        prop="name"
         label="产品经理"
         width="163">
+        <template slot-scope="scope" v-if="scope.supports">
+          {{scope.row.dfile.responsiblePerson}}
+        </template>
       </el-table-column>
       <el-table-column label="指定设计单">
         <template slot-scope="scope">
           <el-button size="mini"
-                     @click="upddiod=true">指定设计单</el-button>
+                     @click="upupddiod(scope.$index)">指定设计单</el-button>
         </template>
       </el-table-column>
     </el-table>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageno"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
   </div>
 </template>
 
@@ -272,12 +338,7 @@
       data(){
           return {
             updstu: {},
-            outapp: [
-              // labourHourAmount:'',
-              // amountUnit:'',
-              // costPrice:'',
-              // subtotal:''
-            ],
+            outapp: [],
             productlist: [],
             upddiod:false,
             pickerOptions1: {
@@ -288,10 +349,48 @@
             sc: false,
             prolist:[],
             show1:true,
-            show2:false
+            show2:false,
+            pageno:1,
+            pagesize:5,
+            total:0,
+            Search:{},
+            firestoption:[],
+            secondoption:[],
+            thirdoption:[],
+            datelist:''
           }
       },
       methods: {
+        //获取数据
+        getdata(){
+          var param = new URLSearchParams();
+          param.append("pageno",this.pageno);
+          param.append("pagesize",this.pagesize);
+          Object.keys(this.Search).forEach(item =>{
+            param.append(item,this.Search[item])
+          })
+          if(this.datelist!=''){
+            param.append("date1",this.datelist[0])
+            param.append("date2",this.datelist[1])
+          }
+
+          this.$axios.post("gx/selDesignsheet",param).then(response =>{
+            this.productlist=response.data.list;
+            this.total=response.data.total;
+          }).catch()
+        },
+        //加载一三级分类
+        getKind(){
+          this.$axios.post("conf/queryAll").then(response =>{
+            this.firestoption=response.data
+          }).catch()
+        },
+        //打开指定设计单的模态框
+        upupddiod(index){
+          this.updstu=this.productlist[index];
+          console.log(this.updstu)
+          this.upddiod=true;
+        },
         //  关闭模态框
         handleClose(done) {
           this.$confirm('确认关闭？')
@@ -308,6 +407,7 @@
             this.sc = true;
           }).catch(e => alert("错误"))
         },
+        //添加工序的方法
         insprolist(index, ids) {
 
           if (this.outapp.length > 0) {
@@ -321,6 +421,7 @@
             this.outapp.push(this.prolist[index]);
           }
         },
+        //删除工序的方法
         delprolist() {
           var newoutapp = this.outapp.filter(item => item.checked == true)
           console.log(newoutapp)
@@ -338,6 +439,7 @@
             });
           }
         },
+        //计算方法
         aa(ids) {
           console.log("blur")
           var row = this.outapp.find((item) => {
@@ -347,6 +449,7 @@
             row.subtotal = Number(row.labourHourAmount) * Number(row.costPrice);
           }
         },
+        //预览
         showUp() {
           this.show1 = false;
           this.show2 = true;
@@ -354,12 +457,90 @@
           //   item.subtotal = Number(item.labourHourAmount) * Number(item.costPrice);
           // })
         },
+        //返回
         showdwon() {
           this.show1 = true;
           this.show2 = false
         },
+        //提交
         goinsprolist(){
-          this.prolist
+          console.log(this.updstu)
+          // this.prolist.forEach(item => {
+          //   item.updstu =this.updstu
+          // })
+          this.outapp[0].updstu=this.updstu
+
+          console.log(this.prolist)
+
+          this.$axios.post("gx/tjgx",JSON.stringify(this.outapp),{headers:{"Content-Type":"application/json"}}).then(response =>{
+            this.$message({
+              message: '提交成功，请去审核',
+              type: 'success'
+            });
+            this.upddiod=false;
+            this.showdwon();
+            this.getdata()
+          }).catch(e =>{
+            this.$message.error('出现问题了，请联系开发人员！');
+          })
+        },
+        handleSizeChange(val) {  //页size变更
+          this.pagesize = val;
+          this.pageno = 1;
+          this.getDfile();
+        },
+        handleCurrentChange(val) {  //页码变更
+          this.pageno = val;
+          this.getDfile();
+        },
+        //根据一级选中的值找到二级
+        firest(val){
+          // console.log(this.Search.firstKindId)
+          if (val==''){
+            this.secondoption=[];
+            this.thirdoption=[];
+            this.Search.secondKindId='';
+            this.Search.thirdKindId='';
+          }
+          this.firestoption.forEach(item =>{
+            if(item.kindId ==val){
+              this.secondoption =item.configFileKinds
+            }
+          })
+        },
+        //根据二级选中的值找到三级
+        second(val){
+          if (val==''){
+            this.thirdoption=[];
+            this.Search.thirdKindId='';
+          }
+          this.secondoption.forEach(item =>{
+            if(item.kindId ==val){
+              this.thirdoption =item.configFileKinds
+            }
+            return;
+          })
+        },
+        bb(val){
+          console.log(val)
+        }
+      },
+      created() {
+          this.getKind();
+          this.getdata();
+
+      },
+      filters:{
+        jibie(val){
+          if (val=="D001-1"){
+            return "高档";
+          }
+          if (val=="D001-2"){
+            return "中档";
+          }
+          if (val=="D001-2"){
+            return "低档";
+          }
         }
       }
     }
